2c7b55d22561c419fba04ad0bcb497260ead10cc
Joe Silva <73418407+jjzsilva9@users.noreply.github.com>
2024-03-07 18:27:44 +0000

    implemented random linear classifier, not perceptron

---
+++ Week 4/week_4.py
@@ -0,0 +1,179 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def linear_classify(x, theta, theta_0):
+    """Uses the given theta, theta_0, to linearly classify the given data x. This is our hypothesis or hypothesis class.
+
+    :param x:
+    :param theta:
+    :param theta_0:
+    :return: 1 if the given x is classified as positive, -1 if it is negative, and 0 if it lies on the hyperplane.
+    """
+    
+    return np.sign((theta.T @ x) + theta_0)
+
+
+def Loss(prediction, actual):
+    """Computes the loss between the given prediction and actual values.
+
+    :param prediction:
+    :param actual:
+    :return: 0 if the values are the same, 1 if they are different
+    """
+    
+    return 0 if prediction == actual else 1
+
+
+def E_n(h, data, labels, L, theta, theta_0):
+    """Computes the error for the given data using the given hypothesis and loss.
+
+    :param h: Hypothesis class, for example a linear classifier.
+    :param data: A d x n matrix where d is the number of data dimensions and n the number of examples.
+    :param labels: A 1 x n matrix with the label (actual value) for each data point.
+    :param L: A loss function to compute the error between the prediction and label.
+    :param theta:
+    :param theta_0:
+    :return:
+    """
+    (d, n) = data.shape
+    error_sum = 0
+    for n in range(len(labels[0])):
+        error_sum += L(h(data[:,n], theta, theta_0), labels[0][n])
+    return error_sum/n
+    
+
+
+def random_linear_classifier(data, labels, params={}, hook=None):
+    """
+
+    :param data: A d x n matrix where d is the number of data dimensions and n the number of examples.
+    :param labels: A 1 x n matrix with the label (actual value) for each data point.
+    :param params: A dict, containing a key T, which is a positive integer number of steps to run
+    :param hook: An optional hook function that is called in each iteration of the algorithm.
+    :return:
+    """
+    k = params.get('k', 100)  # if k is not in params, default to 100
+    (d, n) = data.shape
+
+    minErrorAns = (0, 0)
+    minError = float("inf")
+    for i in range(k):
+        theta = np.random.uniform(low=-2.0, high=2.1, size=d)
+        theta0 = np.random.uniform(low=-5, high=5)
+        if (e:= E_n(linear_classify, data, labels, Loss, theta, theta0)) < minError:
+            minError = e
+            minErrorAns = (theta, theta0)
+        if hook: hook((theta, theta0))
+    return minErrorAns
+
+
+def perceptron(data, labels, params={}, hook=None):
+    """The Perceptron learning algorithm.
+
+    :param data: A d x n matrix where d is the number of data dimensions and n the number of examples.
+    :param labels: A 1 x n matrix with the label (actual value) for each data point.
+    :param params: A dict, containing a key T, which is a positive integer number of steps to run
+    :param hook: An optional hook function that is called in each iteration of the algorithm.
+    :return:
+    """
+    T = params.get('T', 100)  # if T is not in params, default to 100
+    (d, n) = data.shape
+
+    # Todo: Implement the Perceptron algorithm here.
+    pass
+
+
+def plot_separator(plot_axes, theta, theta_0):
+    """Plots the linear separator defined by theta, theta_0, into the given plot_axes.
+
+    :param plot_axes: Matplotlib Axes object
+    :param theta:
+    :param theta_0:
+    """
+
+    # One way we can plot the intercept is to compute the parametric line equation from the implicit form.
+    # compute the y-intercept by setting x1 = 0 and then solving for x2:
+    y_intercept = -theta_0 / theta[1]
+    # compute the slope (-theta[0]/theta[1], I think)
+    slope = -theta[0] / theta[1]
+    # Then compute two points using:
+    xmin, xmax = -15, 15
+    # Note: It's not ideal to only plot the lines in a fixed region, but it makes this code simple for now.
+
+    p1_y = slope * xmin + y_intercept
+    p2_y = slope * xmax + y_intercept
+
+    # Plot the separator:
+    plot_axes.plot([xmin, xmax], [p1_y.flatten()[0], p2_y.flatten()[0]], '-')
+    # Plot the normal:
+    # Note: The normal might not appear perpendicular on the plot if ax.axis('equal') is not set - but it is
+    # perpendicular. Resize the plot window to equal axes to verify.
+    plot_axes.arrow((xmin + xmax) / 2, (p1_y.flatten()[0] + p2_y.flatten()[0]) / 2, float(theta[0]), float(theta[1]))
+
+
+if __name__ == '__main__':
+    """
+    We'll define data X with its labels y, plot the data, and then run either the random_linear_classifier or the
+    perceptron learning algorithm, to find a hypothesis h from the class of linear classifiers.
+    We then plot the best hypothesis, as well as compute the training error. 
+    """
+
+    # Let's create some training data and labels:
+    #   X is a d x n matrix where d is the number of data dimensions and n the number of examples. So each data point
+    #     is a column vector.
+    #   y is a 1 x n matrix with the label (actual value) for each data point.
+    X = np.array([[2, 3, 9, 12],
+                  [5, 2, 6, 5]])
+    y = np.array([[1, -1, 1, -1]])
+
+    # To test your algorithm on a larger dataset, uncomment the following code. It generates uniformly distributed
+    # random data in 2D, along with their labels.
+    X = np.random.uniform(low=-5, high=5, size=(2, 20))  # d=2, n=20
+    y = np.sign(np.dot(np.transpose([[3], [4]]), X) + 6)  # theta=[3, 4], theta_0=6
+
+    # Plot positive data green, negative data red:
+    colors = np.choose(y > 0, np.transpose(np.array(['r', 'g']))).flatten()
+    plt.ion()  # enable matplotlib interactive mode
+    fig, ax = plt.subplots()  # create an empty plot and retrieve the 'ax' handle
+    ax.scatter(X[0, :], X[1, :], c=colors, marker='o')
+    # Set up a pretty 2D plot:
+    ax.set_xlabel('x1')
+    ax.set_ylabel('x2')
+    ax.set_xlim(-20, 20)
+    ax.set_ylim(-20, 20)
+    ax.grid(True, which='both')
+    ax.axhline(color='black', linewidth=0.5)
+    ax.axvline(color='black', linewidth=0.5)
+    ax.set_title("Linear classification")
+
+
+    # We'll define a hook function that we'll use to plot the separator at each step of the learning algorithm:
+    def hook(params):
+        (th, th0) = params
+        plot_separator(ax, th, th0)
+
+
+    # Run the RLC or Perceptron: (uncomment the following lines to call the learning algorithms)
+    theta, theta_0 = random_linear_classifier(X, y, {"k": 100}, hook=None)
+    # theta, theta_0 = perceptron(X, y, {"T": 100}, hook=None)
+    # Plot the returned separator:
+    plot_separator(ax, theta, theta_0)
+    
+
+    # Run the RLC, plot E_n over various k:
+    # Todo: Your code
+    E_nbyk = [[], []]
+    for k in range(1, 100):
+        avg = 0
+        for i in range(10):
+            theta, theta_0 = random_linear_classifier(X, y, {"k": k}, hook=None)
+            avg += E_n(linear_classify, X, y, Loss, theta, theta_0)
+        avg/=10
+        E_nbyk[0].append(k)
+        E_nbyk[1].append(avg)
+    plt.clf()
+    plt.plot(E_nbyk[0], E_nbyk[1])
+    plt.show()
+    print("Finished.")
+    input("end")
+    
